7ce13e20adc76fcf4add5be0dfb4d1aa50cf6cff
JIANGYU-14 <wot.river@gmail.com>
2016-09-30 17:57:38 -0400

    Add files via upload

---
+++ Main-Code-Files/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MealTableViewCell.swift
+//  TESTIOS
+//
+//  Created by JIANGYU  😈 on 9/28/16.
+//  Copyright © 2016 JIANGYU  😈. All rights reserved.
+//
+
+
+import UIKit
+class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+
+}
